b73ed314ae3760df8356dc11112c52b5b54aa67c
Mohadeseh <emamipourmohadeseh@yahoo.com>
2022-06-02 18:34:23 +0430

    w6-Regularized Linear Regression and Bias,Variance
    Week 6 Assignments\Regularized Linear Regression and Bias,Variance

--- featureNormalize.m
+++ featureNormalize.m
@@ -5,35 +5,11 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 %   is 1. This is often a good preprocessing step to do when
 %   working with learning algorithms.
 
-% You need to set these values correctly
-X_norm = X;
-mu = zeros(1, size(X, 2));
-sigma = zeros(1, size(X, 2));
+mu = mean(X);
+X_norm = bsxfun(@minus, X, mu);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: First, for each feature dimension, compute the mean
-%               of the feature and subtract it from the dataset,
-%               storing the mean value in mu. Next, compute the 
-%               standard deviation of each feature and divide
-%               each feature by it's standard deviation, storing
-%               the standard deviation in sigma. 
-%
-%               Note that X is a matrix where each column is a 
-%               feature and each row is an example. You need 
-%               to perform the normalization separately for 
-%               each feature. 
-%
-% Hint: You might find the 'mean' and 'std' functions useful.
-%       
-
-
-mu = mean(X_norm);
 sigma = std(X_norm);
-
-tf_mu = X_norm - repmat(mu,length(X_norm),1);
-tf_std = repmat(sigma,length(X_norm),1);
-
-X_norm = tf_mu ./ tf_std;
+X_norm = bsxfun(@rdivide, X_norm, sigma);
 
 
 % ============================================================
